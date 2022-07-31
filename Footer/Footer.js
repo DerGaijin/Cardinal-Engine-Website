f4c5cceaa1fd6248a62fa3101be9d8272dd0d808
DerGaijin <44649120+DerGaijin@users.noreply.github.com>
2022-08-01 01:33:38 +0200

    Basic Layout

---
+++ Footer/Footer.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", InsertFooterContent);
+
+function InsertFooterContent() {
+    document.getElementById("FooterContent").innerHTML = "This Website is work-in-progress.";
+}
